c2a7bcde1fd3fd8df00ad4ec58914d311be64394
Mayank Lau <33819808+mayanklau@users.noreply.github.com>
2026-05-03 10:41:38 +0530

    Allow production preview CORS origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,8 @@ app.add_middleware(
         "http://127.0.0.1:3001",
         "http://localhost:3002",
         "http://127.0.0.1:3002",
+        "http://localhost:3004",
+        "http://127.0.0.1:3004",
     ],
     allow_credentials=True,
     allow_methods=["*"],
